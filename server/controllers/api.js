d66db95162a0f3c90f8b21aedb97cca6f0bc1c97
Meshari <98meshari@gmail.com>
2021-08-17 14:47:20 +1000

    Setting up API file

---
+++ server/controllers/api.js
@@ -0,0 +1,6 @@
+const express = require('express')  
+const database = require('../database')
+const User = database.UserSchema;
+const Favour = database.FavourSchema;
+
+const apiRouter = express.Router()

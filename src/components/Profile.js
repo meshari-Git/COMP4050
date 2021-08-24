25e0d87568e2b9d197576dc71acfd51b6dd0cb8e
Benjamin Fricke <benjamin.fricke@students.mq.edu.au>
2021-08-24 15:18:28 +1000

    Sample code in profile to use login service

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,7 @@ import "../assets/css/profile.css";
 import "../assets/css/sidebar.css";
 import "../assets/css/sidebarnav.css";
 import "../assets/css/userinfo.css";
+import userService from '../services/user.js';
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import { isAuthenticated } from "../authentication/apiindex";
@@ -16,6 +17,29 @@ function Profile({user, setUser}) {
           lastName: "Fricke",
           id: "aldsfkjfklaj",
           email:"ben@ben.com"}
+
+    const sendLogin = (event) => {
+
+      event.preventDefault()      
+
+      userService.login("ben@rtt.co", "badpassword")
+        .then(response => {
+              
+          if(!response || response === null) {
+            alert("Username or Password are incorrect")
+            return
+          }
+          console.log(response)
+        })
+        .catch(err => {
+            console.log(err)
+            alert("Username or Password are incorrect")
+            return
+        })
+
+    }
+
+    
 
   
     if(user === undefined || user.firstName === undefined ) { //user not logged in
@@ -53,7 +77,7 @@ function Profile({user, setUser}) {
         </Row>
 
             <h5>Jobs</h5>
-            <button className="btn btn-primary"> + Add </button>            
+            <button className="btn btn-primary" onClick={(event) => sendLogin(event)}> + Add </button>            
             <Table bordered hover striped>
               <thead>
                 <tr>
@@ -108,101 +132,3 @@ function Profile({user, setUser}) {
   }
 
 export default Profile;
-
-
-
-
-
-
-// const Profile = () => {
-//   // const {
-//   //   user: { _id, name, email, address, balance, about, role },
-//   // } = isAuthenticated();
-  
-//   // const {
-//   //   user: { name, email, address, balance},
-//   // } = isAuthenticated();
-
-  
-
-//   // let rating = 0;
-
-//   // fetch("http://localhost:3000/rating?total=true&chosenUserID=" + _id)
-//   //   .then((resp) => resp.json())
-//   //   .then((data) => {
-//   //     rating = data.total;
-//   //   });
-
-//   return (
-//     <div className="Profile-page">
-      
-//       <h1>Hello Ben</h1>
-      
-//       <div className="sidebar">
-//         <div className="card-body">
-//           <div className="sidebarnav">
-//             <Link to="/">
-//               <h2 className="titles">Search for Jobs</h2>
-//             </Link>
-//           </div>
-//           <div className="sidebarnav">
-//             <Link to="/changeinfo">
-//               <h2 className="titles">Change Personal Information</h2>
-//             </Link>
-//           </div>
-
-//           <div className="sidebarnav">
-//             <Link to="/dashboard">
-//               <h2 className="titles">Dashboard</h2>
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="userinfo">
-//         <div className="card">
-//           <div className="card-body">
-//             <h5 className="card-title">Hello {name},</h5>
-//             <p className="card-text">View or change your information.</p>
-//           </div>
-//         </div>
-
-//         <div className="card-group">
-//           <div className="card">
-//             <div className="card-header">Account Information</div>
-//             <div className="row">
-//               <div className="col-sm-6">
-//                 <div className="card-body">
-//                   <blockquote className="blockquote mb-0">
-//                     <h5>Contact Information</h5>
-//                     <p>{name}</p>
-//                     <p>{email}</p>
-//                   </blockquote>
-//                 </div>
-//               </div>
-//               <div className="col-sm-6">
-//                 <div className="card-body">
-//                   <blockquote className="blockquote mb-0">
-//                     <h5>Address</h5>
-//                     <p>{address}</p>
-//                   </blockquote>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div class="card">
-//           <div class="card-body">
-//             <h5 clas="card-title">Your Balance</h5>
-//           </div>
-//           <blockquote className="blockquote balance">
-//             <h5>${balance}</h5>
-//           </blockquote>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Profile;

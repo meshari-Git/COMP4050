253b67ae54ede2eca0646d9ca670975fd348ef77
Ner0theHer0 <malachi@mashiah.net>
2021-09-28 18:04:02 +1000

    sort by date drop down

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/homepage.css";
-
+import { Dropdown } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import GMap from "../components/GMap.js";
@@ -14,30 +14,54 @@ class HomePage extends Component {
 
     this.state = {
       searchData: '',
+      dropDownState: "Sort by"
     };
   }
 
 
   updateSearchField = (event) => {
-    this.setState({...this.state.searchData, searchData: event.target.value})
+    this.setState({ ...this.state.searchData, searchData: event.target.value })
+  }
+
+  MyFunction() {
+    console.log("lel")
   }
 
   render() {
-    
+
 
     return (
       <div className="homePage">
         <div className="homeContainer">
-          <form className="searchBar">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              id="Search"
-              onChange={this.updateSearchField}
-            />
-          </form>
+          <div className="ctr">
+            <div className="sb">
+              <form className="searchBar">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  id="Search"
+                  onChange={this.updateSearchField}
+                />
+              </form>
+            </div>
+            <div className = "dd">
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  {this.state.dropDownState}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item as="button" >Newest First</Dropdown.Item>
+                  <Dropdown.Item href="#" >Oldest First</Dropdown.Item>
+
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </div>
+
+
           <div className="filters"></div>
           <div><Favour filter={this.state.searchData} /></div>
         </div>

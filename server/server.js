1686ca903ca676ef518327ad969171da97b92278
Meshari <98meshari@gmail.com>
2021-11-03 00:23:22 +1100

    deploy

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@
 require('dotenv').config()
 const app = require('./app')
 
-const port = (process.env.PORT || 3001)
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
